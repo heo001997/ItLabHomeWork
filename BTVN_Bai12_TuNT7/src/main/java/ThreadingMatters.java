d88d934ad6af096cca46bc87eab3c7ac87ccf597
TuNT7 <heo001997@gmail.com>
2019-04-07 19:47:21 +0700

    Update B2 Interrupt Thread

---
+++ BTVN_Bai12_TuNT7/src/main/java/ThreadingMatters.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadingMatters {
+    public static void main(String[] args) {
+        ArrayBlockingQueue<RunnableModel> threadQueue = new ArrayBlockingQueue<>(5);
+
+        RunnableModel runnableModel1 = new RunnableModel("1st");
+        RunnableModel runnableModel2 = new RunnableModel("2nd");
+
+        try{
+            runnableModel1.start();
+
+            TimeUnit.SECONDS.sleep(5);
+            if (runnableModel1.isAlive()){
+                runnableModel1.interrupt();
+                System.out.println("Kill this thread now!");
+            }
+            runnableModel2.start();
+
+        } catch (InterruptedException ie){
+            System.out.println("Sleep In Main Interrupted");
+        }
+
+        System.out.println("==> Main thread end!!!");
+    }
+}
